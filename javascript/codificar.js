2eeb6228dbf6f97aa5df1cb7c7da06959e6413bc
Edwin <erodriguezloyaga@gmail.com>
2022-01-31 10:15:22 -0500

    Modificación tecla encriptar

--- javascript/codificar.js
+++ javascript/codificar.js
@@ -1,27 +1,6 @@
-var palabra_codificada = "";
-
-function codificar(msj) {
-    for (var letra = 0; letra < msj.length; letra++) {
-        
-        if (msj[letra] == "e") {
-            palabra_codificada = palabra_codificada + "enter";
-        } else if (msj[letra] == "a") {
-            palabra_codificada = palabra_codificada + "ai";
-        } else if (msj[letra] == "i") {
-            palabra_codificada = palabra_codificada + "imes";
-        } else if (msj[letra] == "o") {
-            palabra_codificada = palabra_codificada + "ober";
-        } else if (msj[letra] == "u") {
-            palabra_codificada = palabra_codificada + "ufat";
-        }
-        else {
-            palabra_codificada = palabra_codificada + msj[letra];
-        } 
-    } 
-    document.getElementById("mensaje_desifrado").value = palabra_codificada;   
-}
-
-function enviar_texto(){
-    var texto = document.getElementById("mensaje_por_descifrar").value;
-    codificar(texto);
+function botonEncriptar(){
+    var encriptar = document.getElementById('mensaje_por_descifrar').value;
+    var encriptado = encriptar.replace(/e/gi, 'enter').replace(/i/gi, 'imes').replace(/a/gi, 'ai').replace(/o/gi, 'ober').replace(/u/gi, 'ufat');
+    document.getElementById("mensaje_desifrado").value = encriptado
+    document.getElementById("mensaje_por_descifrar").value="";
 }
